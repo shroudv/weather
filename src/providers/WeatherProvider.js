13e21478cbc311481fb04eba1d40cdd3998074d1
Ahmad Ahmadov <76842244+shroudv@users.noreply.github.com>
2022-09-05 15:56:28 +0400

    Update WeatherProvider.js

--- src/providers/WeatherProvider.js
+++ src/providers/WeatherProvider.js
@@ -8,7 +8,7 @@ function WeatherProvider(props) {
     const [getWeather, setWeather] = useState();
 
     useEffect(() => {
-        axios.get('https://api.weatherapi.com/v1/forecast.json?key=1ac528524a5444c5bc473959223107&q=Baku&days=8&aqi=no&alerts=no')
+        axios.get('https://api.weatherapi.com/v1/forecast.json?key=d423e172db324a45ade115433220509&q=Baku&days=8&aqi=no&alerts=no')
             .then(({ data }) => setWeather(data))
             .catch((err) => console.error(err))
     }, [])
